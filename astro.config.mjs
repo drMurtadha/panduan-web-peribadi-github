8d492881e66a2143695eabb3e11b9a6b524dac14
Mohd Murtadha <45449425+drMurtadha@users.noreply.github.com>
2026-09-21 15:42:41 +0800

    Tambah konfigurasi dan dependensi Astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://drmurtadha.github.io',
+  base: '/panduan-web-peribadi-github',
+  build: {
+    inlineStylesheets: 'always',
+  },
+});
